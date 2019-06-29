96c371bfb92ac302de264fbf237a1b6767bee6f9
Aaron Whang <tlbhibiki@gmail.com>
2019-06-28 17:00:08 -0700

    It will create a web page to take in a user's birth date and desired planet as input, then calculate their age on that planet and their life expectancy.

--- src/main.js
+++ src/main.js
@@ -4,20 +4,77 @@ import './styles.css';
 import $ from 'jquery';
 
 
+const minutes = 60 * 1000;
+const hours = 60 * minutes;
+const days = 24 * hours;
+const mercuryYear = 88 * days;
+const venusYear = 225 * days;
+const earthYear = 365 * days;
+const marsYear = 687 * days;
+const jupiterYear = 4332 * days;
+const saturnYear = 10759 * days;
+const uranusYear = 30688 * days;
+const neptuneYear = 60182 * days;
+
+function lifeExpectancy(milliage,planetConstant)
+{
+  let lifeExp = 80 * earthYear;
+  if (lifeExp < milliage) {
+    $("#gal-out").append("<br>You should be dead!");
+  } else {
+    let timeLeft = lifeExp - milliage;
+    $("#gal-out").append("<br>You have " + parseFloat(Math.round(timeLeft / planetConstant).toFixed(2)) + " years left to live!");
+  }
+}
+
+function planetConst(milliage,planet) {
+  if (planet === "Mercury") {
+    $("#gal-out").html("You are " + parseFloat(Math.round(milliage / mercuryYear).toFixed(2)) + " years old on the planet " + planet);
+    lifeExpectancy(milliage,mercuryYear);
+  }
+  if (planet === "Venus") {
+    $("#gal-out").html("You are " + parseFloat(Math.round(milliage / venusYear).toFixed(2)) + " years old on the planet " + planet);
+    lifeExpectancy(milliage,venusYear);
+  }
+  if (planet === "Earth") {
+    $("#gal-out").html("You are " + parseFloat(Math.round(milliage / earthYear).toFixed(2)) + " years old on the planet " + planet);
+    lifeExpectancy(milliage,earthYear);
+  }
+  if (planet === "Mars") {
+    $("#gal-out").html("You are " + parseFloat(Math.round(milliage / marsYear).toFixed(2)) + " years old on the planet " + planet);
+    lifeExpectancy(milliage,marsYear);
+  }
+  if (planet === "Jupiter") {
+    $("#gal-out").html("You are " + parseFloat(Math.round(milliage / jupiterYear).toFixed(2)) + " years old on the planet " + planet);
+    lifeExpectancy(milliage,jupiterYear);
+  }
+  if (planet === "Saturn") {
+    $("#gal-out").html("You are " + parseFloat(Math.round(milliage / saturnYear).toFixed(2)) + " years old on the planet " + planet);
+    lifeExpectancy(milliage,saturnYear);
+  }
+  if (planet === "Uranus") {
+    $("#gal-out").html("You are " + parseFloat(Math.round(milliage / uranusYear).toFixed(2)) + " years old on the planet " + planet);
+    lifeExpectancy(milliage,uranusYear);
+  }
+  if (planet === "Neptune") {
+    $("#gal-out").html("You are " + parseFloat(Math.round(milliage / neptuneYear).toFixed(2)) + " years old on the planet " + planet);
+    lifeExpectancy(milliage,neptuneYear);
+  }
+}
+
 
 $(function(){
 
 
-  // $("#playercon0").click(function() {
-  //   $(".topbar").html("");
-  //   $(".topbar").removeClass("redtext");
-  //   attacking(char1,char2,char3,turn,battle.monster[0]);
-  //   monsterTurn(turn);
-  //   printStatus(char1,char2,char3);
-  //   turn += 1;
-  //   turnHidden();
-  //   setTimeout(function(){ turnUI(turn); printButtons(); }, 250);
-  // });
+$("#gal-page form").submit(function() {
+  event.preventDefault();
+  var planet = $("input:radio[name=planet]:checked").val();
+  let d = new Date(document.getElementById("born").value);
+  let millidob = d.getTime();
+  let millicurrent = Date.now();
+  let milliage = millicurrent - millidob;
+  planetConst(milliage,planet)
+  });
 
 
 });
